b22672b6cf21f8e198f8b120a8230187154f1135
asgared <alejandro.mendiola.colin@outlook.com>
2024-03-03 15:24:56 -0600

    first page index

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+// import Navigation from "@/components/Navigation";
+import NavigationLogo from "@/components/NavigationLogo";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,8 +18,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="es">
+      <head>
+        <link rel="stylesheet" href="https://bootswatch.com/5/quartz/bootstrap.min.css" />
+      </head>
+      <body className={inter.className}>
+        {/* <Navigation /> */}
+        <NavigationLogo />
+        {children}
+      </body>
     </html>
   );
 }
