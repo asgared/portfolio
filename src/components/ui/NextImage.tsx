4bc19dd6730c65b004d46d8254c0a09d1a2551eb
Alejandro Mendiola <alejandro.mendiola.colin@outlook.com>
2025-10-17 18:53:51 -0600

    fix issue image to deply

--- src/components/ui/NextImage.tsx
+++ src/components/ui/NextImage.tsx
@@ -1,9 +1,12 @@
 import NextImageBase, { ImageProps as NextImageProps } from 'next/image'
-import { chakra, forwardRef } from '@chakra-ui/react'
+import {
+  chakra,
+  forwardRef,
+  type ChakraProps,
+  type ComponentWithAs,
+} from '@chakra-ui/react'
 
-type NextChakraImageProps = NextImageProps & {
-  rounded?: string
-}
+type NextChakraImageProps = NextImageProps & Omit<ChakraProps, 'fill'>
 
 const ChakraNextImage = chakra(NextImageBase, {
   shouldForwardProp: (prop) =>
@@ -22,7 +25,7 @@ const ChakraNextImage = chakra(NextImageBase, {
       'style',
       'onLoadingComplete',
     ].includes(prop),
-})
+}) as ComponentWithAs<'img', NextChakraImageProps>
 
 export const NextImage = forwardRef<NextChakraImageProps, 'img'>((props, ref) => {
   return <ChakraNextImage ref={ref} {...props} />
